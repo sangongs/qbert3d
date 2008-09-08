5de02ea208f22c9017121bd1d4e92fa757e6e648
haimbender <haimbender@97581c8e-fe54-0410-aa42-dd6ba39e7182>
2008-09-08 22:37:20 +0000

    Forgot to commit the main.cpp

--- Src/Apps/App/Main.cpp
+++ Src/Apps/App/Main.cpp
@@ -7,14 +7,18 @@
 #include "Rastering/Point3D.h"
 #include "Rastering/SimpleControler.h"
 #include "Rastering/DiamondQbertModel.h"
+#include "Rastering/QbertView.h"
 #include "Rastering/ArielView.h"
 #include "Rastering/FPSView.h"
+#include "Rastering/DoubleQbertView.h"
 
 int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrev*/, LPSTR /*szCmdLine*/, int /*sw*/)
 	{
-	BGComplete::DiamondQbertModel model(4, "RedBox", "BlueBox", "Qbert", 10.0f);
-	BGComplete::FPSView view;
-	BGComplete::SimpleControler controler((BGComplete::View*)&view, (BGComplete::QbertModel*)&model);
+	using namespace BGComplete;
+	DiamondQbertModel model(4, "RedBox", "BlueBox", "Qbert", 10.0f);
+	
+	DoubleQbertView view(QbertView_Ptr(new ArielView()), QbertView_Ptr(new FPSView()), 0.1f);
+	SimpleControler controler((QbertView*)&view, (QbertModel*)&model);
 
 	controler.Run(1000, 1000, 8);
 		
