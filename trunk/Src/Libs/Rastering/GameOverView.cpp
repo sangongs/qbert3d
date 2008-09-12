f129f950be5de4f5599ad7c46acb8d4bb16f7bf7
haimbender <haimbender@97581c8e-fe54-0410-aa42-dd6ba39e7182>
2008-09-12 14:10:17 +0000

    adding objects

--- trunk/Src/Libs/Rastering/GameOverView.cpp
+++ trunk/Src/Libs/Rastering/GameOverView.cpp
@@ -12,8 +12,7 @@
 namespace BGComplete
 {
 	GameOverView::GameOverView() : _gameOverFont("FONTS\\CALIBRI.TTF", 28), _pressNFont("FONTS\\CALIBRI.TTF", 28)
-	{
-	}
+	{}
 
 	void GameOverView::CameraMove(float /*deltaX*/, float /*deltaY*/, float /*deltaZ*/, float /*xRotate*/, float /*yRotate*/, float /*zRotate*/, char /*viewKey*/) {}
 
